222710d9b6b0a3ed473331e544c028db0071060b
Joash10 <163319696+Joash10@users.noreply.github.com>
2024-09-20 11:30:28 -0300

    ´
    ´

---
+++ 3 trimestre/perguntas.js
@@ -0,0 +1,44 @@
+criaCartao(
+    'serie',
+    'qual a sinopse de Stranger Things?',
+    'Quando um rapaz desaparece, uma pequena vila descobre um mistério relacionado com experiências secretas, assustadoras forças sobrenaturais e uma estranha rapariga'
+)
+
+criaCartao(
+    'serie',
+    'orthen banks quais os personages',
+    'john b, jj,cameron, sara cameron,kiara,topper,ward cameron,pope,cleo, entre outros'
+)
+
+criaCartao(
+    'maquiagem',
+    'uma famosa maquiagem?',
+    'max love'
+)
+
+criaCartao(
+    'comidas',
+    'qual o chocolate mais vendido da lacta?',
+    'diamante negro'
+)
+criaCartao(
+    'desenhos',
+    'sobre o anime naruto?',
+    'A série gira em torno das aventuras vividas por Naruto Uzumaki, um jovem órfão habitante da Aldeia da Folha que sonha em se tornar o quinto Hokage, o maior guerreiro e governante da vila. '
+)
+criaCartao(
+    'serie',
+    'star vs a forca do mar, nome do namora da star?',
+    'tom,um demonio '
+)
+criaCartao(
+    'Gravity Falls: Um Verão de Mistérios ',
+     ' quais os personagens principais?',
+    'dipper, mabel,'
+)
+
+criaCartao(
+    'serie',
+    'qual é o nome do criador que criou o round 6 ?',
+    'Hwang Dong-hyuk'
+)
